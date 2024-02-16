3e500a1f143d89712081923a17b4ce2d90ef0d8f
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2024-02-16 13:08:44 +0700

    add yolo integration with tello

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import cv2
 from djitellopy import tello
+from ultralytics import YOLO
+import math
+
 
 def process_tello_video(drone):
     # for use in main with djitellopy object
@@ -9,7 +12,6 @@ def process_tello_video(drone):
         if cv2.waitKey(1) & 0xFF == ord('q') :
             break
     cv2.destroyAllWindows()
-    print(drone.get_battery())
     drone.end()
 
 
@@ -19,8 +21,68 @@ def main():
     # Establish connection with drone
     drone.connect()
     # Start drones camera stream 
-    drone.stream_on()
-    process_tello_video(drone)
+    drone.streamon()
+    print(drone.get_battery())
+    yolo(drone)
+    print(drone.get_battery())
+
+def yolo(drone):
+    # model
+    model = YOLO("yolo-Weights/yolov8n.pt")
+
+    # object classes
+    classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                "teddy bear", "hair drier", "toothbrush"
+                ]
+
+
+    while True:
+        img = drone.get_frame_read().frame
+        results = model(img, stream=True)
+
+        # coordinates
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # put box in cam
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->",confidence)
+
+                # class name
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls] + "-" + str(confidence), org, font, fontScale, color, thickness)
+
+        cv2.imshow('Webcam', img)
+        if cv2.waitKey(10) == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    drone.end()
 
 if __name__ == '__main__':
     main() 
